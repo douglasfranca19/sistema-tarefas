4f6bafdb0408f635befbbc3067007cea6e43280b
douglasfranca19 <francadouglas19@gmail.com>
2024-02-05 23:48:07 -0300

    Método PUT funcionando, mas faltam muitos ajustes

--- src/main/java/br/com/dhome/sistematarefas/controller/TarefasController.java
+++ src/main/java/br/com/dhome/sistematarefas/controller/TarefasController.java
@@ -1,12 +1,15 @@
 package br.com.dhome.sistematarefas.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,15 +30,28 @@ public class TarefasController {
 
 	}
 
+	@GetMapping(value = "/busca-id/{id}")
+	public ResponseEntity<Optional<TarefasEntitie>> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
+
+	}
+
 	@GetMapping(value = "/busca-todos")
 	public ResponseEntity<List<TarefasEntitie>> findAll() {
 		return ResponseEntity.ok().body(service.findAll());
 
 	}
 
-	@DeleteMapping(value = "/deleta-tdos")
+	@DeleteMapping(value = "/deleta-todos")
 	public void deleteAllTarefas(TarefasEntitie tarefasEntitie) {
 		service.deleteAllTarefas(tarefasEntitie);
+	}
+
+	@PutMapping(value = "/atualiza-tarefa/{id}")
+	public ResponseEntity<TarefasEntitie> atualizaTarefa(@PathVariable Long id,
+			@RequestBody TarefasEntitie tarefasEntitie) {
+		return ResponseEntity.ok().body(service.atualizaTarefa(id, tarefasEntitie));
+
 	}
 
 }
--- src/main/java/br/com/dhome/sistematarefas/service/TarefasService.java
+++ src/main/java/br/com/dhome/sistematarefas/service/TarefasService.java
@@ -1,12 +1,14 @@
 package br.com.dhome.sistematarefas.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.dhome.sistematarefas.entities.TarefasEntitie;
 import br.com.dhome.sistematarefas.repository.TarefasRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class TarefasService {
@@ -19,12 +21,34 @@ public class TarefasService {
 
 	}
 
+	public Optional<TarefasEntitie> findById(Long id) {
+		return tarefas.findById(id);
+
+	}
+
 	public List<TarefasEntitie> findAll() {
 		return tarefas.findAll();
 	}
 
 	public void deleteAllTarefas(TarefasEntitie tarefasEntitie) {
 		tarefas.deleteAll();
+	}
+
+	@Transactional
+	public TarefasEntitie atualizaTarefa(Long id, TarefasEntitie tarefasEntitie) {
+
+		TarefasEntitie tEntitie = tarefas.getReferenceById(id);
+		atulizaTarefa(tEntitie, tarefasEntitie);
+		return tarefas.save(tEntitie);
+
+	}
+
+	private void atulizaTarefa(TarefasEntitie tEntitie, TarefasEntitie tarefasEntitie) {
+
+		tEntitie.setTipoTarefa(tarefasEntitie.getTipoTarefa());
+		tEntitie.setDescricaoTarefa(tarefasEntitie.getDescricaoTarefa());
+		tEntitie.setDataTarefa(tarefasEntitie.getDataTarefa());
+		tEntitie.setStatusTarefa(tarefasEntitie.getStatusTarefa());
 	}
 
 }
